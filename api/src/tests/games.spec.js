a90371aa6d9feac7d346e20a2dc15ca337aea2fb
Cristinaizquierdo2020 <cristinaizquierdo2000@gmail.com>
2022-01-31 12:37:52 +0100

    test(test): add a few tests about the games

--- api/src/tests/games.spec.js
+++ api/src/tests/games.spec.js
@@ -1,30 +1,38 @@
 import app, { server } from '../app.js';
 import supertest from 'supertest';
 import dotenv from 'dotenv';
+import { connection } from '../db/mysql.js';
+
 dotenv.config();
 
 beforeAll(() => {
   console.log('BEFORE ALL TEST');
 });
 
-beforeEach(() => {
+beforeEach(async () => {
   console.log('BEFORE EACH test');
   server.close();
+  await connection.query('Delete from games');
 });
 
-afterAll(() => {
+afterAll(async () => {
   console.log('AFTER ALL test');
   server.close();
+  await connection.query('SET FOREIGN_KEY_CHECKS = 0');
+  await connection.query('TRUNCATE TABLE games');
+  await connection.query('SET FOREIGN_KEY_CHECKS = 1');
 });
 
 describe('Tests API REST games', () => {
+  const query = `INSERT INTO games (title) VALUES ('test')`;
   test('get all', async () => {
+    await connection.query(query);
     await supertest(app)
       .get('/api/games')
       .expect(200)
       .then((response) => {
         const games = response.body;
-        expect(games.length).toBe(0);
+        expect(games.length).toBe(1);
       });
   });
 });
